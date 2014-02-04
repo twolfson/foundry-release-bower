5e839f573a7e581236482c0ad8f178139461968b
Todd Wolfson <todd@twolfson.com>
2014-02-04 01:40:30 -0800

    Copied over library, modified test, and dependencies

--- test/foundry-release-bower_test.js
+++ test/foundry-release-bower_test.js
@@ -1,11 +1,24 @@
-var foundry_release_bower = require('../');
+// Load in dependencies
+var fs = require('fs');
+var expect = require('chai').expect;
+var bowerRelease = require('../');
+var fixtureUtils = require('./utils/fixtures');
 
-describe('foundry-release-bower', function () {
-  before(function () {
+// Define our test
+describe('Publishing', function () {
+  describe('in a bower module', function () {
+    var fixtureDir = fixtureUtils.fixtureDir('bower');
+    before(function release (done) {
+      bowerRelease.publish({
+        version: '0.1.0',
+        message: 'Release 0.1.0',
+        description: null
+      }, done);
+    });
 
-  });
-
-  it('', function () {
-
+    it('updates the bower.json', function () {
+      var pkgJson = fs.readFileSync(fixtureDir + '/bower.json');
+      expect(JSON.parse(pkgJson)).to.have.property('version', '0.1.0');
+    });
   });
 });
