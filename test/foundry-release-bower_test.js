382abe768b02ce695c1aeb2e79cec844cec9584c
Todd Wolfson <todd@twolfson.com>
2014-10-12 19:08:41 +0900

    Moved to setVersion over updateFiles

--- test/foundry-release-bower_test.js
+++ test/foundry-release-bower_test.js
@@ -5,11 +5,11 @@ var bowerRelease = require('../');
 var fixtureUtils = require('./utils/fixtures');
 
 // Define our test
-describe('Setting the version', function () {
+describe('Updating files', function () {
   describe('in a bower module', function () {
     var fixtureDir = fixtureUtils.fixtureDir('bower');
     before(function release (done) {
-      bowerRelease.setVersion({
+      bowerRelease.updateFiles({
         version: '0.1.0',
         message: 'Release 0.1.0',
         description: null
